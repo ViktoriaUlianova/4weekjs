88b92dc04b70ff82e1450520931a3a8f932ead87
Viktoria <viktoria.ulianova@mail.ru>
2021-05-29 10:35:12 +0700

    calc done

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+function sumInput() {
+
+    let numbers = [];
+
+    while (true) {
+
+        let value = prompt("Введите число", []);
+
+        if (value === "" || value === null || !isFinite(value)) break;
+
+        numbers.push(+value);
+    }
+
+    numbers.sort();
+
+
+    let sum = 0;
+    for (let number of numbers) {
+        sum += number;
+    }
+    return numbers;
+}
+
+alert(sumInput());
